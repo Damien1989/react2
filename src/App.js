0a96cc84bc2d8e22365d134db205b284a855f0a3
damien <contact.damiendiaz@gmail.com>
2023-10-25 20:39:45 +0200

    routes pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import {Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Menu from './components/Menu';
-import './css/app.css';
 import TechnoAdd from './pages/TechnoAdd';
 import TechnoList from './pages/TechnoList';
+import './css/app.css';
 
 function App() {
   return (
-    <div>
+    <>
     <Menu />
-   <Home />
-   <TechnoAdd />
-   <TechnoList />
-   </div>
+      <Routes>
+        <Route path="/" element= {<Home /> } />
+        <Route path="/add" element= {<TechnoAdd /> } />
+        <Route path="/list" element= {<TechnoList /> } />
+      </Routes>
+   </>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export default function Menu() {
     return(
         <div className="menu">
         <ul>
-            <li>Accueil</li>
-            <li>Ajouter techno</li>
-            <li>Toutes les technos</li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/add">Ajouter Techno</NavLink></li>
+            <li><NavLink to="/list">Toutes les technos</NavLink></li>
+          
         </ul>
         </div>
     );
