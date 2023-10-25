c49e236c2e3766863e583e95fef8817aca3c4319
damien <contact.damiendiaz@gmail.com>
2023-10-25 21:01:44 +0200

    active link routes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,9 +4,10 @@ export default function Menu() {
     return(
         <div className="menu">
         <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/add">Ajouter Techno</NavLink></li>
-            <li><NavLink to="/list">Toutes les technos</NavLink></li>
+            <li><NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : undefined)}>Home</NavLink></li>
+            <li><NavLink to="/add" className={({ isActive }) => (isActive ? "active-link" : undefined)}>Ajouter une techno</NavLink></li>
+            <li><NavLink to="/list" className={({ isActive }) => (isActive ? "active-link" : undefined)}>Toutes les Technos</NavLink></li>
+           
           
         </ul>
         </div>
